1cd8709e7b4be3e3b82b7d14c640543d22a60c63
anegassi <89844342+anegassi@users.noreply.github.com>
2021-10-15 06:22:57 -0700

    update

---
+++ Jscript/data.js
@@ -0,0 +1,148 @@
+export default{
+    products:[
+{
+    id:'1',
+    name:'bubble gun',
+    category:'kids',
+    image:'images/bubblegun.png',
+    price: 59,
+    brand:'toys',
+    rating: 4.5,
+    numReviews: 10,
+    
+
+},
+{
+    id:'2',
+    name:'camera',
+    category:'electronics',
+    image:'images/camera.png',
+    price: 75,
+    brand:'canion',
+    rating: 4.8,
+    numReviews: 9,
+    
+
+},
+{
+    id:'3',
+    name:'coffee table',
+    category:'furniture',
+    image:'images/coffetable.png',
+    price: 109,
+    brand:'ikea',
+    rating: 4.2,
+    numReviews: 8,
+    
+
+},
+{
+    id:'4',
+    name:'console',
+    category:'kids',
+    image:'images/console.png',
+    price: 45,
+    brand:'toys',
+    rating: 3.2,
+    numReviews: 6,
+    
+
+},
+{
+    id:'5',
+    name:'kidsgame',
+    category:'kids',
+    image:'images/kidsgame.png',
+    price: 39,
+    brand:'toys',
+    rating: 4.2,
+    numReviews: 8,
+    
+
+},
+{
+    id:'6',
+    name:'sumsung phone',
+    category:'electronics',
+    image:'images/phone.png',
+    price: 350,
+    brand:'sumsung',
+    rating: 4.9,
+    numReviews: 10,
+    
+
+},
+{
+    id:'7',
+    name:'shelf',
+    category:'furniture',
+    image:'images/shelf.png',
+    price: 89,
+    brand:'wayfair',
+    rating: 3.7,
+    numReviews: 7,
+    
+
+},
+{
+    id:'8',
+    name:'sofa',
+    category:'furniture',
+    image:'images/sofa.png',
+    price: 189,
+    brand:'ikea',
+    rating: 3.3,
+    numReviews: 5,
+    
+
+},
+{
+    id:'9',
+    name:'tool1',
+    category:'tools',
+    image:'images/tool1.png',
+    price: 48,
+    brand:'dremel',
+    rating: 3.9,
+    numReviews: 6,
+    
+
+},{
+    id:'10',
+    name:'tool2',
+    category:'tools',
+    image:'images/tools2.png',
+    price: 65,
+    brand:'max tool',
+    rating: 2.8,
+    numReviews: 5,
+    
+
+},
+{
+    id:'11',
+    name:'vaccum',
+    category:'electronics',
+    image:'images/vaccum.png',
+    price: 79,
+    brand:'shark navigator',
+    rating: 3.9,
+    numReviews: 8,
+    
+
+},
+{
+    id:'12',
+    name:'watch',
+    category:'electronics',
+    image:'images/watch.png',
+    price: 62,
+    brand:'quartz',
+    rating: 4.7,
+    numReviews: 7,
+    
+
+},
+
+    ]
+};
---
+++ Jscript/productList.js
@@ -0,0 +1,37 @@
+//import populateList from "./buildList.js";
+import data from "./data.js";
+window.onload=router;
+function router() {
+     const main=document.getElementById('main-container');
+     main.innerHTML=populateList.render();
+};
+
+const populateList= {
+     render: ()=>{
+         const { products } = data;
+         return `
+         <ul class="products">
+            ${products.map(
+                (product) =>`
+                <li>
+                    <div class="product">
+                       <a href="/#/product/${product.id}">
+                       <img src="${product.image}" alt="${product.name}" />
+                       </a>
+                     <div class="product-name">
+                       <a href="/#/product/${product.id}">${product.name}</a>
+                     </div>
+                     <div class="product-brand">${product.brand}</div>
+                     <div class="product-price">$${product.price}</div>
+                    </div> 
+ 
+                </li>
+                `
+            )
+         .join('\n')}
+ 
+         `
+     }
+ }
+
+//window.addEventListener('load',router);
